d5372b5439870cd9742e5e27248ce2142283800d
skyprincegamer <akgamer3108@gmail.com>
2025-12-10 17:38:28 +0530

    add rectangle function

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,15 @@
 use audio_recorder_rs::Recorder;
-use macroquad::color::{GREEN, WHITE};
-use macroquad::shapes::draw_line;
+use macroquad::color::{GREEN, RED, WHITE};
+use macroquad::shapes::{draw_line, draw_rectangle};
 use macroquad::window::next_frame;
 use macroquad::window::{clear_background, screen_height, screen_width};
 use std::collections::VecDeque;
 use std::sync::{Arc, Mutex};
 use std::thread;
 
-const ALPHA: f32 = 0.1;
-const VOLUME_SCALE: f32 = 16.0;
+
+const ALPHA: f32 = 0.5;
+const VOLUME_SCALE: f32 = 2.;
 #[macroquad::main("Texture")]
 async fn main() {
     let mut recorder = Recorder::new();
@@ -42,12 +43,13 @@ async fn main() {
                 *samples_len.lock().unwrap() = locked.len();
             }
             for (index, x) in drawn.iter_mut().enumerate() {
-                *x = (drawn_prev[index] * (1.- ALPHA) + *x  * ALPHA);
+                *x = drawn_prev[index] * (1.- ALPHA) + *x  * ALPHA;
             }
-            draw_lines(&drawn , mid);
+
             for (index, x) in drawn.iter().enumerate() {
                 drawn_prev[index] = *x;
             }
+            draw_lines(&drawn, mid);
         }
         else{
             draw_lines(&drawn_prev, mid);
@@ -60,6 +62,14 @@ async fn main() {
 fn draw_lines(drawn_prev: &Vec<f32>, mid: f32) {
     for i in 0..drawn_prev.len() - 1 {
         let i_f = i as f32;
-        draw_line(i_f, mid - drawn_prev[i] * mid, i_f + 1., mid - drawn_prev[i + 1] * mid, 2., GREEN);
+        draw_line(i_f, mid - drawn_prev[i] * mid, i_f + 1., mid - drawn_prev[i + 1] * mid, 2., macroquad::color::RED);
+    }
+}
+
+fn draw_rectangles(bars : &Vec<f32>){
+    for i in 0..bars.len(){
+        let i_f = i as f32;
+        let rec_width = screen_width()/bars.len() as f32;
+        draw_rectangle(i_f*rec_width ,screen_height()- bars[i] , rec_width , bars[i] , RED );
     }
 }
