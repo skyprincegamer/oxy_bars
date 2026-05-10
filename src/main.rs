dbc7c07b00486e6b54b9b959cb761475da9c85ca
skyprincegamer <akgamer3108@gmail.com>
2026-05-10 10:12:52 +0530

    rebirth 2.0

--- src/main.rs
+++ src/main.rs
@@ -1,121 +1,184 @@
 mod utils;
 
-use audio_recorder_rs::Recorder;
-use macroquad::color::WHITE;
+use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
+use macroquad::color::BLACK;
 use macroquad::input::is_quit_requested;
+use macroquad::window::clear_background;
 use macroquad::window::next_frame;
-use macroquad::window::{clear_background, screen_width};
-use spectrum_analyzer::windows::hann_window;
-use spectrum_analyzer::{samples_fft_to_spectrum, Frequency, FrequencyLimit};
+use rustfft::FftPlanner;
+use rustfft::num_complex::Complex;
+use serde::{Deserialize, Serialize};
 use std::collections::VecDeque;
+use std::fs::File;
+use std::io::{Read, Write};
+use std::sync::atomic::AtomicBool;
+use std::sync::atomic::Ordering::SeqCst;
 use std::sync::{Arc, Mutex};
 use std::thread;
 use std::time::Duration;
 
-const SAMPLE_TIME :f64 = 1_000_000. / 44100.;
-const FFT_SIZE : usize = 2048;
+#[derive(Deserialize, Serialize, Debug)]
+struct Config {
+    fft: usize,
+    alpha: f32,
+    color_start: Vec<f32>,
+    color_end: Vec<f32>,
+    color_final: Vec<f32>,
+    scale : u32
+}
 
-const GLOBAL_SCALE : f32 = 5.;
-const ALPHA :f32 = 0.5;
-const COLOR_START : (f32, f32, f32) = (1., 0.5, 0.);
-const COLOR_END : (f32, f32, f32) = (0.5, 1.0, 0.);
-const COLOR_FINAL : (f32, f32, f32) = (0.5, 0.5, 1.0);
+impl Default for Config {
+    fn default() -> Self {
+        Config {
+            fft: 2048,
+            alpha: 0.5,
+            color_start: vec![1.0, 0.5, 0.0],
+            color_end: vec![0.5, 1.0, 0.0],
+            color_final: vec![0.5, 0.5, 1.0],
+            scale :2,
+        }
+    }
+}
 
 #[macroquad::main("Oxy Bars")]
 async fn main() {
-    let mut recorder = Recorder::new();
-    let receiver = recorder.start(true).expect("Failed to start recording");
+    let mut config_str = String::new();
+    let config_file = File::open("config.toml");
+    let mut erred = false;
+    match config_file {
+        Ok(mut file) => {
+            let res = file.read_to_string(&mut config_str);
+            match res {
+                Ok(_) => {}
+                Err(_) => {
+                    config_str = "".to_string();
+                    erred = true
+                }
+            }
+        }
+        Err(_) => {
+            config_str = "".to_string();
+            erred = true
+        }
+    }
+    dbg!(erred);
+    let config: Config;
+    let config_res = toml::from_str(config_str.as_str());
+    match config_res {
+        Ok(x) => config = x,
+        Err(_) => {
+            erred = true;
+            config = Config::default()
+        }
+    }
+    dbg!(&config);
+    if erred {
+        let mut f = File::create("config.toml").unwrap();
+        f.write_all(toml::to_string(&config).unwrap().as_bytes())
+            .unwrap();
+        dbg!("file written!");
+    }
+    let device = cpal::default_host()
+        .default_input_device()
+        .expect("NO MIC BRUH");
+    let device_config = device.default_input_config().expect("NO MIC CONFIG BRUH");
     let samples = Arc::new(Mutex::new(VecDeque::new()));
     let samples_clone_fft = samples.clone();
-    let noise_profile = Arc::new(Mutex::new(Vec::<f32>::with_capacity(FFT_SIZE)));
+    let noise_profile = Arc::new(Mutex::new(Vec::<f32>::with_capacity(config.fft)));
     let noise_profile_clone = noise_profile.clone();
-    let mut noise_profiling_done = false;
-    thread::spawn(move || {
-        while let Ok(d) = receiver.recv() {
-            while !noise_profiling_done && noise_profile.lock().unwrap().len() < FFT_SIZE  {
-                let mut locked = noise_profile.lock().unwrap();
-                for sample in d.clone() {
-                    if locked.len() < FFT_SIZE {
-                        locked.push(sample);
+    let noise_recording_done = Arc::new(AtomicBool::new(false));
+    let noise_recording_done_clone = noise_recording_done.clone();
+    let noise_spectrum_done = Arc::new(AtomicBool::new(false));
+    let noise_spectrum_done_clone = noise_spectrum_done.clone();
+
+    //recorder stream
+    let stream = device
+        .build_input_stream(
+            &device_config.into(),
+            move |data: &[f32], _: &cpal::InputCallbackInfo| {
+                if !noise_recording_done.load(SeqCst) {
+                    let mut noise = noise_profile.lock().unwrap();
+                    for &sample in data {
+                        if noise.len() < config.fft {
+                            noise.push(sample);
+                        }
+                    }
+                    if noise.len() >= config.fft {
+                        noise_recording_done.store(true, SeqCst);
+                    }
+                } else {
+                    let mut locked = samples.lock().unwrap();
+                    for &sample in data {
+                        locked.push_back(sample);
                     }
                 }
-            }
-            noise_profiling_done = true;
-            for sample in d {
-                samples.lock().unwrap().push_back(sample);
-            }
-        }
-    });
-    let spectrum_mutex :Arc<Mutex<Vec<f32>>> = Arc::new(Mutex::new(vec![0. ; 44100/2]));
+            },
+            |err| eprintln!("stream error: {err}"),
+            None,
+        )
+        .expect("Failed to build input stream");
+
+    stream.play().expect("Failed to start stream");
+    let spectrum_mutex: Arc<Mutex<Vec<f32>>> = Arc::new(Mutex::new(vec![0.; config.fft]));
     let spectrum_mutex_clone = spectrum_mutex.clone();
-    thread::spawn(move || loop {
-        thread::sleep(Duration::from_micros(SAMPLE_TIME as u64));
-        let mut input = vec![0.; FFT_SIZE];
-        if samples_clone_fft.lock().unwrap().len() > (FFT_SIZE+2) {
-            {
-                let mut locked = samples_clone_fft.lock().unwrap();
-                for i in 0..FFT_SIZE {
-                    input[i] = locked.pop_front().unwrap() ;
-                }
-            }
-            let mut spectrum_noise = None;
-            if noise_profile_clone.lock().unwrap().len() >= (FFT_SIZE) {
-                let locked_noise = noise_profile_clone.lock().unwrap();
-                let windowed_noise = hann_window(&locked_noise);
-                spectrum_noise = Some(samples_fft_to_spectrum(&windowed_noise,
-                                                             44100, //sampling rate
-                                                             FrequencyLimit::All, None).unwrap());
-            }
-            else{
+
+    //fft thread
+    let mut planner = FftPlanner::new();
+    let fft = planner.plan_fft_forward(config.fft);
+    thread::spawn(move || {
+        let mut noise_noise = vec![];
+        loop {
+            thread::sleep(Duration::from_micros(22)); // 1/44100 s = 22 µs
+
+            let mut buffer = vec![Complex { re: 0.0, im: 0.0 }; config.fft];
+
+            if !noise_spectrum_done.load(SeqCst) && noise_recording_done_clone.load(SeqCst) {
+                let mut locked = noise_profile_clone.lock().unwrap();
+                buffer = locked.iter().map(|&x| Complex { re: x, im: 0. }).collect();
+                fft.process(&mut buffer);
+                noise_noise = buffer.iter().map(|&x| x.re).collect();
+                noise_spectrum_done.store(true, SeqCst);
+            } else if !noise_recording_done_clone.load(SeqCst) {
                 continue;
-            }
-
-            let windowed_input = hann_window(&input);
-            let spectrum = samples_fft_to_spectrum(&windowed_input,
-                44100, //sampling rate
-               FrequencyLimit::All, None).unwrap();
-
-            {
+            } else {
+                let mut locked = samples_clone_fft.lock().unwrap();
+                if locked.len() < config.fft + 2 {
+                    continue;
+                }
+                for i in 0..config.fft {
+                    buffer[i] = Complex {
+                        re: locked.pop_front().unwrap() ,
+                        im: 0.,
+                    };
+                }
+                fft.process(&mut buffer);
                 let mut locked = spectrum_mutex_clone.lock().unwrap();
-                let indexer = |x:Frequency| (x.val() as usize).min(44100/2 - 1);
-                let noise_iter = spectrum_noise.as_ref().unwrap().data().iter();
-                for (&(freq, freq_val) , &(noise_freq , noise_val)) in spectrum.data().iter().zip(noise_iter) {
-                    if indexer(noise_freq) == indexer(freq){
-                        (*locked)[indexer(freq)] = (freq_val.val() - noise_val.val()).max(0.0);
-                    }
-                    else{
-                        (*locked)[indexer(freq)] = freq_val.val();
-                    }
-                    
+                for i in 0..config.fft{
+                    locked[i] = (buffer[i].norm() - noise_noise[i]).max(0.0);
                 }
             }
         }
     });
-    let mut drawn_prev = vec![0.; screen_width() as usize];
+    let disp_w = config.fft / 2 - (config.fft/100) * 2;
+    let mut drawn_prev = vec![0.; disp_w];
     loop {
-
-        clear_background(WHITE);
-        if drawn_prev.len() != screen_width() as usize {
-            drawn_prev = vec![0.; screen_width() as usize];
+        clear_background(BLACK);
+        if !noise_spectrum_done_clone.load(SeqCst) {
+            continue;
         }
         let locked = spectrum_mutex.lock().unwrap().clone();
-        let drawn = utils::interpolate_the_bars
-                                (utils::scale_the_bars(
-                                utils::average_the_bars(locked , screen_width() as usize)) , &drawn_prev, ALPHA);
-        drawn_prev = drawn.clone();
-        utils::draw_rectangles(drawn);
-        match macroquad::input::get_char_pressed(){
+        drawn_prev = utils::draw_rectangles(&locked, &drawn_prev, &config);
+        match macroquad::input::get_char_pressed() {
             None => {}
             Some(key) => {
-                if key == char::from_u32(27).unwrap(){
+                if key == char::from_u32(27).unwrap() {
                     break;
                 }
             }
         }
-        if is_quit_requested(){
+        if is_quit_requested() {
             break;
         }
         next_frame().await;
     }
-    recorder.stop();
 }
